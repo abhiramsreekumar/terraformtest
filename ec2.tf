9dc6009703d842a5eb6a3e0b62ecf3b73fd51207
Abhiram Sreekumar <31365613+abhiramsreekumar@users.noreply.github.com>
2023-09-20 09:59:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,9 @@ resource "aws_instance" "testnew" {
   ami           = var.aminame
   instance_type = var.instancetyp
   key_name      = var.mynewkeys
-  count         = 3
 
 
   tags = {
-    name = "testec2${count.index}"
+    name = "testec2"
   }
 }
