7268ee8438b09c5d8189b18a0d97fbb6c87a7209
Abhiram Sreekumar <31365613+abhiramsreekumar@users.noreply.github.com>
2023-09-26 15:18:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,36 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-
     }
-
   }
 }
 provider "aws" {
-  region  = var.reg
+  region  = "us-east-1"
   assume_role {
 role_arn = "arn:aws:iam::268003200000:role/terraform"
 
 }
+}
+variable "mynewkey" {
+  type    = string
+  default = "mynewkey"
+}
+resource "aws_security_group" "demo-sg" {
+  name = "sec-grp"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
