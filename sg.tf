a10b132ea77bd191c703aba68cf4be397f7bd5a3
Abhiram Sreekumar <31365613+abhiramsreekumar@users.noreply.github.com>
2023-09-27 11:33:54 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,10 +1,17 @@
-resource "aws_instance" "testnew" {
-  ami           = "ami-04cb4ca688797756f"
-  instance_type = "t2.micro"
-  key_name      = "mynewkey"
+resource "aws_security_group" "demo-sg" {
+  name = "sec-grp"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  tags = {
-    name = "testec1"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
